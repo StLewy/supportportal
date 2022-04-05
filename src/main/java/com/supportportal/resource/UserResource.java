78c0d266c5f45747c2eb8eada2bb23b378bf42bf
slewandowski <st.lewy@gmail.com>
2022-04-05 22:56:50 +0200

    adding exception

--- src/main/java/com/supportportal/resource/UserResource.java
+++ src/main/java/com/supportportal/resource/UserResource.java
@@ -1,15 +1,16 @@
 package com.supportportal.resource;
 
+import com.supportportal.exception.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserResource {
+@RequestMapping(value = {"/","/user"})
+public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
-    public String showUser(){
-        return "Application works";
+    public String showUser() {
+        return "application works";
     }
 }
